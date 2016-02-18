cbd86c1c28f9ac3921d73143556eb806bdf923b7
Eugen Auschew <eugen@dhcp-172-26-15-106.dynamic.tarent.de>
2016-02-18 12:49:47 +0100

    initial commit

---
+++ bomberman-server/start.go
@@ -0,0 +1,30 @@
+package main
+
+import "net"
+import "fmt"
+import "bufio"
+import "strings" // only needed below for sample processing
+
+func main() {
+
+  fmt.Println("Launching server...")
+
+  // listen on all interfaces
+  ln, _ := net.Listen("tcp", ":5000")
+
+  // accept connection on port
+  conn, _ := ln.Accept()
+
+  // run loop forever (or until ctrl-c)
+  for {
+    // will listen for message to process ending in newline (\n)
+    message, _ := bufio.NewReader(conn).ReadString('\n')
+    // output message received
+    fmt.Print("Message Received:", string(message))
+
+    // sample process for string received
+    newMessage := strings.ToUpper(message)
+    // send new string back to client
+    conn.Write([]byte(newMessage + "\n"))
+  }
+}
