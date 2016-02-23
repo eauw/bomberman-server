a08b1fa8f8f3215ba4727419baf018f0586cea24
Eugen Auschew <e.auschew@tarent.de>
2016-02-23 10:48:28 +0100

    Jetzt können sich mehrere Clients gleichzeitig verbinden.

--- bomberman-server/start.go
+++ bomberman-server/start.go
@@ -10,11 +10,19 @@ func main() {
   fmt.Println("Launching tcp server...")
 
   // listen on all interfaces
-  ln, _ := net.Listen("tcp", fmt.Sprintf(":%d",port))
+  ln, _ := net.Listen("tcp", fmt.Sprintf(":%d", port))
   fmt.Printf("Listening tcp on port %d\n", port)
 
-  // accept connection on port
-  conn, _ := ln.Accept()
+  for {
+    // accept connection on port
+    conn, _ := ln.Accept()
+    if conn != nil {
+      go newClientConnected(conn)
+    }
+  }
+}
+
+func newClientConnected(conn net.Conn) {
   fmt.Printf("client %s connected\n", conn.RemoteAddr())
 
   // run loop forever (or until ctrl-c)
@@ -22,9 +30,11 @@ func main() {
     messageBytes, _, _ := bufio.NewReader(conn).ReadLine()
     messageString := string(messageBytes)
     // output message received
+    fmt.Println("----------------")
+    fmt.Printf("Message from client: %s\n", conn.RemoteAddr())
     fmt.Printf("Message Received:%s\n", messageString)
 
-    // if message is "quit" server will stop
+    // if message is "quit" server will close connection
     if handleMessage(messageString, conn) == false {
       return
     }
